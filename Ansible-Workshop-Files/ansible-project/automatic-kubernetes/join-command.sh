e993e44641cecc872274fb5733e6c63f74a4663b
Piotr Szewczuk <piotr.szewczuk@gmail.com>
2021-07-06 02:55:48 +0200

    Workshop Files

---
+++ Ansible-Workshop-Files/ansible-project/automatic-kubernetes/join-command.sh
@@ -0,0 +1,1 @@
+kubeadm join 192.168.20.12:6443 --token abks5r.g8y4rdaztv612dh1 --discovery-token-ca-cert-hash sha256:7f42ef761e04ed666d264d2f7898397877175a344b35d8ae2764c4fea770652f 
---
+++ Ansible-Workshop-Files/ansible-project/automatic-kubernetes/roles/kubernetes-worker/files/join-command.sh
@@ -0,0 +1,1 @@
+kubeadm join 192.168.20.12:6443 --token 5kz9cv.h7w1wo1ynxg8gl3n --discovery-token-ca-cert-hash sha256:7ec98d4c08cd4900508b228359fc3b7e3231c91f17ba0af2adb9f0fe6efa07b8 
